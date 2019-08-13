a29f46f91206add33bed2b7745cc1f0dc355146c
Jason Ruhlin <jasonruhlin@Jasons-MBP.fios-router.home>
2019-08-12 22:09:36 -0400

    Move github repo actions into index

--- src/daisy-cli.ts
+++ src/daisy-cli.ts
@@ -23,15 +23,10 @@ export class DaisyCLI {
         @inject('Changelog') private changelog: Changelog) {
         this.logger.showTitleAndBanner();
         this.executeDaisyCLI();
-        this.executeGithubRepo();
     }
 
     public async executeDaisyCLI(): Promise<any> {
         return this.githubActions();
-    }
-
-    public async executeGithubRepo(): Promise<any> {
-        return this.repoActions();
     }
 
     private async githubActions() {
@@ -75,88 +70,5 @@ export class DaisyCLI {
                 return this.pullRequest.generateFile();
             }
         }
-    }
-
-    private async repoActions() {
-
-        const chalk = require('chalk');
-        const clear = require('clear');
-        const figlet = require('figlet');
-
-        const github = require('./utils/github-util');
-        const repo = require('./utils/repo.util');
-        const files = require('./utils/files.util');
-
-        clear();
-        console.log(
-            chalk.yellow(
-                figlet.textSync('Ginit', { horizontalLayout: 'full' })
-            )
-        );
-
-        if (files.directoryExists('.git')) {
-            console.log(chalk.red('Already a git repository!'));
-            process.exit();
-        }
-
-        const getGithubToken = async () => {
-            // Fetch token from config store
-            let token = github.getStoredGithubToken();
-            if (token) {
-                return token;
-            }
-
-            // No token found, use credentials to access github account
-            await github.setGithubCredentials();
-
-            // Check if access token for ginit was registered
-            const accessToken = await github.hasAccessToken();
-            if (accessToken) {
-                console.log(chalk.yellow('An existing access token has been found!'));
-                // ask user to regenerate a new token
-                token = await github.regenerateNewToken(accessToken.id);
-                return token;
-            }
-
-            // No access token found, register one now
-            token = await github.registerNewToken();
-            return token;
-        }
-
-        const run = async () => {
-            try {
-                // Retrieve & Set Authentication Token
-                const token = await getGithubToken();
-                github.githubAuth(token);
-
-                // Create remote repository
-                const url = await repo.createRemoteRepo();
-
-                // Create .gitignore file
-                await repo.createGitignore();
-
-                // Setup local repository and push to remote
-                const done = await repo.setupRepo(url);
-                if (done) {
-                    console.log(chalk.green('All done!'));
-                }
-            } catch (err) {
-                if (err) {
-                    switch (err.code) {
-                        case 401:
-                            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
-                            break;
-                        case 422:
-                            console.log(chalk.red('There already exists a remote repository with the same name'));
-                            break;
-                        default:
-                            console.log(err);
-                    }
-                }
-            }
-        }
-
-        run();
-
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,13 @@ import { Changelog } from './templates/universal/changelog.template';
 
 export function index(): DaisyCLI {
   const container: Container = new Container();
+  const chalk = require('chalk');
+  const clear = require('clear');
+  const figlet = require('figlet');
+
+  const github = require('./utils/github-util');
+  const repo = require('./utils/repo.util');
+  const files = require('./utils/files.util');
 
   // Utils
   container.bind<Logger>('Logger').to(Logger).inSingletonScope();
@@ -39,6 +46,77 @@ export function index(): DaisyCLI {
 
   // DaisyCLI
   container.bind<DaisyCLI>('DaisyCLI').to(DaisyCLI).inSingletonScope();
+
+  clear();
+  console.log(
+    chalk.yellow(
+      figlet.textSync('Ginit', { horizontalLayout: 'full' })
+    )
+  );
+
+  if (files.directoryExists('.git')) {
+    console.log(chalk.red('Already a git repository!'));
+    process.exit();
+  }
+
+  const getGithubToken = async () => {
+    // Fetch token from config store
+    let token = github.getStoredGithubToken();
+    if (token) {
+      return token;
+    }
+
+    // No token found, use credentials to access github account
+    await github.setGithubCredentials();
+
+    // Check if access token for ginit was registered
+    const accessToken = await github.hasAccessToken();
+    if (accessToken) {
+      console.log(chalk.yellow('An existing access token has been found!'));
+      // ask user to regenerate a new token
+      token = await github.regenerateNewToken(accessToken.id);
+      return token;
+    }
+
+    // No access token found, register one now
+    token = await github.registerNewToken();
+    return token;
+  }
+
+  const run = async () => {
+    try {
+      // Retrieve & Set Authentication Token
+      const token = await getGithubToken();
+      github.githubAuth(token);
+
+      // Create remote repository
+      const url = await repo.createRemoteRepo();
+
+      // Create .gitignore file
+      await repo.createGitignore();
+
+      // Setup local repository and push to remote
+      const done = await repo.setupRepo(url);
+      if (done) {
+        console.log(chalk.green('All done!'));
+      }
+    } catch (err) {
+      if (err) {
+        switch (err.code) {
+          case 401:
+            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+            break;
+          case 422:
+            console.log(chalk.red('There already exists a remote repository with the same name'));
+            break;
+          default:
+            console.log(err);
+        }
+      }
+    }
+  }
+
+  run();
 
   return container.get<DaisyCLI>('DaisyCLI');
 };
