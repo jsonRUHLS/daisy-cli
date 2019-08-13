8475ca3dd1c0cd6b0198cfe7d929e48056aa1e72
Jason Ruhlin <jasonruhlin@Jasons-MBP.fios-router.home>
2019-08-12 21:39:09 -0400

    adding repo actions

--- src/daisy-cli.ts
+++ src/daisy-cli.ts
@@ -5,26 +5,32 @@ import { BugReport, FeatureRequest, PullRequest } from './templates/github';
 import { UniversalChoiceValue, GithubChoiceValue, Answer } from './models/choice';
 import { githubFileQuestion } from './questions';
 import { ConsoleMessage } from './models/console-message';
+import { } from '.'
 
 @injectable()
 export class DaisyCLI {
 
     constructor(@inject('Logger') private logger: Logger,
-                @inject('CodeOfConduct') private codeOfConduct: CodeOfConduct,
-                @inject('License') private license: License,
-                @inject('Contributing') private contributing: Contributing,
-                @inject('BugReport') private bugReport: BugReport,
-                @inject('FeatureRequest') private featureRequest: FeatureRequest,
-                @inject('PullRequest') private pullRequest: PullRequest,
-                @inject('ToDo') private todo: ToDo,
-                @inject('Readme') private readme: Readme,
-                @inject('Changelog') private changelog: Changelog) {
+        @inject('CodeOfConduct') private codeOfConduct: CodeOfConduct,
+        @inject('License') private license: License,
+        @inject('Contributing') private contributing: Contributing,
+        @inject('BugReport') private bugReport: BugReport,
+        @inject('FeatureRequest') private featureRequest: FeatureRequest,
+        @inject('PullRequest') private pullRequest: PullRequest,
+        @inject('ToDo') private todo: ToDo,
+        @inject('Readme') private readme: Readme,
+        @inject('Changelog') private changelog: Changelog) {
         this.logger.showTitleAndBanner();
         this.executeDaisyCLI();
+        this.executeGithubRepo();
     }
 
     public async executeDaisyCLI(): Promise<any> {
         return this.githubActions();
+    }
+
+    public async executeGithubRepo(): Promise<any> {
+        return this.repoActions();
     }
 
     private async githubActions() {
@@ -69,9 +75,87 @@ export class DaisyCLI {
             }
         }
     }
-}
 
-@injectable()
-export class InitGit {
-    
+    private async repoActions() {
+
+        const chalk = require('chalk');
+        const clear = require('clear');
+        const figlet = require('figlet');
+
+        const github = require('./utils/github-util');
+        const repo = require('./utils/repo.util');
+        const files = require('./utils/files.util');
+
+        clear();
+        console.log(
+            chalk.yellow(
+                figlet.textSync('Ginit', { horizontalLayout: 'full' })
+            )
+        );
+
+        if (files.directoryExists('.git')) {
+            console.log(chalk.red('Already a git repository!'));
+            process.exit();
+        }
+
+        const getGithubToken = async () => {
+            // Fetch token from config store
+            let token = github.getStoredGithubToken();
+            if (token) {
+                return token;
+            }
+
+            // No token found, use credentials to access github account
+            await github.setGithubCredentials();
+
+            // Check if access token for ginit was registered
+            const accessToken = await github.hasAccessToken();
+            if (accessToken) {
+                console.log(chalk.yellow('An existing access token has been found!'));
+                // ask user to regenerate a new token
+                token = await github.regenerateNewToken(accessToken.id);
+                return token;
+            }
+
+            // No access token found, register one now
+            token = await github.registerNewToken();
+            return token;
+        }
+
+        const run = async () => {
+            try {
+                // Retrieve & Set Authentication Token
+                const token = await getGithubToken();
+                github.githubAuth(token);
+
+                // Create remote repository
+                const url = await repo.createRemoteRepo();
+
+                // Create .gitignore file
+                await repo.createGitignore();
+
+                // Setup local repository and push to remote
+                const done = await repo.setupRepo(url);
+                if (done) {
+                    console.log(chalk.green('All done!'));
+                }
+            } catch (err) {
+                if (err) {
+                    switch (err.code) {
+                        case 401:
+                            console.log(chalk.red('Couldn\'t log you in. Please provide correct credentials/token.'));
+                            break;
+                        case 422:
+                            console.log(chalk.red('There already exists a remote repository with the same name'));
+                            break;
+                        default:
+                            console.log(err);
+                    }
+                }
+            }
+        }
+
+        run();
+
+    }
 }
